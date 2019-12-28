4546e35cc2efb25567e342fa485471c7f796de07
kiingdavies <daviesadedeji007@gmail.com>
2019-12-29 00:28:27 +0100

    Clock Buildup

---
+++ my_clock/lib/clock_face.dart
@@ -0,0 +1,48 @@
+import 'clock_text.dart';
+import 'package:flutter/material.dart';
+import 'clock_dial_painter.dart';
+import 'clock_hands.dart';
+
+class ClockFace extends StatelessWidget{
+
+  final DateTime dateTime;
+  final ClockText clockText;
+  ClockFace({this.clockText = ClockText.arabic, this.dateTime});
+
+  @override
+  Widget build(BuildContext context) {
+    return new Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: new AspectRatio(
+        aspectRatio: 1.0,
+        child: new Container(
+          width: double.infinity,
+          decoration: new BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.white,
+          ),
+
+          child: new Stack(
+            children: <Widget>[
+              //dial and numbers
+              new Container(
+                width: double.infinity,
+                height: double.infinity,
+                padding: const EdgeInsets.all(10.0),
+                child:new CustomPaint(
+                  painter: new ClockDialPainter(clockText: clockText),
+                ),
+              ),
+
+              new ClockHands(dateTime:dateTime),
+
+            ],
+          ),
+        ),
+
+      ),
+    );
+  }
+}
+
+
